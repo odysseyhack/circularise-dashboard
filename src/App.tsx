94b3e869ebcfa034d0adeb73deef5be539c212ad
Pedram <pedram.ard@milvum.com>
2019-04-12 20:40:54 +0200

    Added Chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,80 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Line } from 'react-chartjs-2';
 
-import { LoginPage } from './pages/LoginPage';
 import styles from './styles/app.module.scss';
 
 class App extends Component {
   public render() {
     return (
       <div className={styles.app}>
-        <Router>
-          <Route path="/login" component={LoginPage} />
-        </Router>
+        <Line
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+              {
+                label: 'My First dataset',
+                fill: '+2',
+                lineTension: 0.1,
+                backgroundColor: 'rgba(0,0,0,0.4)',
+                borderColor: 'rgba(255,0,0,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+              },
+              {
+                label: 'My First dataset',
+                fill: false,
+                lineTension: 0.1,
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [70, 60, 85, 90, 60, 60, 40],
+              },
+              {
+                label: 'My Second dataset',
+                fill: false,
+                lineTension: 0.1,
+                borderColor: 'rgba(0,255,0,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [75, 69, 90, 91, 66, 65, 50],
+              },
+            ],
+          }}
+        />
       </div>
     );
   }
